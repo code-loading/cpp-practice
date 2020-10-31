a9857783758d325fbf7181744449b6d2a4641719
Ehsan <eahmed711@gmail.com>
2020-10-31 22:30:13 +0100

    cin and pow

--- ConsoleProject1/ConsoleProject1.cpp
+++ ConsoleProject1/ConsoleProject1.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-using namespace std;
-
+#include <cmath>
+using std::cout;
+using std::cin;
 
 int main()
 {
-
-    
+    /*
+    int slices;
+    cout << "Yo how many slices of pizza you gon eat?" << std::endl;
+    cin >> slices;
+    cout << "You have " << slices << " slices of pizza!" << std::endl;
+    cout << std::endl;
+    */
+    int base, exponent;
+    cout << "What's your base?:" << std::endl;
+    cin >> base;
+    cout << "What's your exponent?:" << std::endl;
+    cin >> exponent;
+    double power = pow(base, exponent);
+    cout << power << std::endl;
 
     system("pause>0");
 }
