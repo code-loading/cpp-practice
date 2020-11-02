a352c1c6db13928baccf71fa36345bbe9612bd46
Ehsan <eahmed711@gmail.com>
2020-11-02 15:25:52 +0100

    emptied to use as base for future branches

--- ConsoleProject1/ConsoleProject1.cpp
+++ ConsoleProject1/ConsoleProject1.cpp
@@ -3,45 +3,10 @@
 using std::cout;
 using std::cin;
 
-//double power(double, int);
-double power(double base, int exponent) {
-
-    double result = 1;
-
-    for (int i = 0; i < exponent; i++) {
-        result = result * base;
-    }
-
-    return result;
-}
-
-void print_pow(double base, int exponent) {
-
-    double myPower = power(base, exponent);
-    cout << base <<" raised to the power "<< exponent <<" is "<< myPower << ".\n";
-}
 
 int main()
 {
-    /*
-    int slices;
-    cout << "Yo how many slices of pizza you gon eat?" << std::endl;
-    cin >> slices;
-    cout << "You have " << slices << " slices of pizza!" << std::endl;
-    cout << std::endl;
-    */
-    int base, exponent;
-    cout << "What's your base?:" << std::endl;
-    cin >> base;
-    cout << "What's your exponent?:" << std::endl;
-    cin >> exponent;
-    cout<<std::endl;
-
-    print_pow(base, exponent);
-    print_pow(100, 4);
-    print_pow(4, 2);
-
-   
+       
 
     system("pause>0");
 }
