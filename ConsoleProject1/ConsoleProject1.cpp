eab3b69270c9fc9f3193b978913e6f67ff44de01
Ehsan <eahmed711@gmail.com>
2020-11-13 22:10:25 +0100

    if user exists return index, otherwise add user to vector

--- ConsoleProject1/ConsoleProject1.cpp
+++ ConsoleProject1/ConsoleProject1.cpp
@@ -26,20 +26,55 @@ public:
 
 };
 
+int add_user_if_not_exist(std::vector<User> users, User user) {
+    for (int i = 0; i < users.size(); i++)
+    {
+        if (users[i].first_name==user.first_name &&
+            users[i].last_name==user.last_name)
+        {
+            return i;
+        }
+    }
+    users.push_back(user);
+    return users.size() - 1;
+}
 
 int main()
 {
-    User me;
-    me.first_name = "Ehsan";
-    me.last_name = "Ahmed";
+    //User me;
+    //me.first_name = "Ehsan";
+    //me.last_name = "Ahmed";
     //me.status = "awesome";
 
     std::vector<User> users;
-    users.push_back(me);
-    users.push_back(User());
+    User user1, user2, user3;
+    
+    user1.first_name = "Ehsan";
+    user1.last_name = "Ahmed";
 
-    std::cout << users[0].first_name << "\n";
-    std::cout << users[1].first_name << "\n";
+    user2.first_name = "Arsalan";
+    user2.last_name = "Ahmed";
+
+    user3.first_name = "Sumit";
+    user3.last_name = "Bhardwaj";
+
+    users.push_back(user1);
+    users.push_back(user2);
+    users.push_back(user3);
+
+    User user4;
+    user4.first_name = "Arsalan";
+    user4.last_name = "Ahmed";
+
+    std::cout << add_user_if_not_exist(users, user4) << std::endl;
+
+    std::cout << users.size() << std::endl;
+
+    //users.push_back(me);
+    //users.push_back(User());
+
+    //std::cout << users[0].first_name << "\n";
+    //std::cout << users[1].first_name << "\n";
 
     //std::cout << "first name: " << me.first_name << "\n";
     //std::cout << "last name: " << me.last_name << "\n";
