caba11eb5f570ec73f5cce5c0841e984eb02f812
Ehsan <eahmed711@gmail.com>
2020-11-13 21:42:19 +0100

    struct

--- ConsoleProject1/ConsoleProject1.cpp
+++ ConsoleProject1/ConsoleProject1.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
 
-
+struct User //default is public for the member values
+{
+    std::string first_name;
+    std::string last_name;
+    std::string get_status() {
+        return status;
+    }
+private:
+    std::string status="Gold"; //assigned a default value
+};
 
 int main()
 {
-       
+    User me;
+    me.first_name = "Ehsan";
+    me.last_name = "Ahmed";
+    //me.status = "awesome";
 
-    system("pause>0");
+    std::cout << "first name: " << me.first_name << "\n";
+    std::cout << "last name: " << me.last_name << "\n";
+    std::cout << "status: " << me.get_status()<< "\n";
+
+    //system("pause>0");
 }
